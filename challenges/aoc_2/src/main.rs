a7a27c30eeeb9869e80470af2d9917d460112270
Mathias Kindberg <mathias.kindberg@gmail.com>
2021-12-30 22:01:06 +0100

    Day two done

--- challenges/aoc_2/src/main.rs
+++ challenges/aoc_2/src/main.rs
@@ -1,3 +1,73 @@
+use std::io::prelude::*;
+
+#[derive(Debug)]
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+impl From<&String> for Direction {
+    fn from(item: &String) -> Self {
+        match item.as_str() {
+            "forward" => Self::Forward,
+            "down" => Self::Down,
+            "up" => Self::Up,
+            _ => panic!("Unknown direction"),
+        }
+    }
+}
+
+fn input() -> Vec<(Direction, usize)> {
+    let stdin = std::io::stdin();
+    stdin
+        .lock()
+        .lines()
+        .into_iter()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            line.split_whitespace()
+                .map(|item| item.to_string())
+                .collect::<Vec<String>>()
+        })
+        .filter_map(|items| Some(((&items[0]).into(), items[1].parse::<usize>().ok()?)))
+        .collect()
+}
+
+fn one(input: &Vec<(Direction, usize)>) -> usize {
+    let (horizontal, depth) = input
+        .iter()
+        .fold((0, 0), |(mut horizontal, mut depth), input| {
+            match input.0 {
+                Direction::Forward => horizontal += input.1,
+                Direction::Up => depth -= input.1,
+                Direction::Down => depth += input.1,
+            }
+            (horizontal, depth)
+        });
+    horizontal * depth
+}
+
+fn two(input: &Vec<(Direction, usize)>) -> usize {
+    let (horizontal, depth, _) =
+        input
+            .iter()
+            .fold((0, 0, 0), |(mut horizontal, mut depth, mut aim), input| {
+                match input.0 {
+                    Direction::Forward => {
+                        horizontal += input.1;
+                        depth += input.1 * aim
+                    }
+                    Direction::Up => aim -= input.1,
+                    Direction::Down => aim += input.1,
+                }
+                (horizontal, depth, aim)
+            });
+    horizontal * depth
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = input();
+    println!("One {}", one(&input));
+    println!("Two {}", two(&input));
 }
