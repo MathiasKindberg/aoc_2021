2b851652fc22c8ed6cd744efce1abdceeeb26f80
Mathias Kindberg <mathias.kindberg@gmail.com>
2021-12-31 13:35:34 +0100

    day three challenge one done

--- challenges/aoc_3/src/main.rs
+++ challenges/aoc_3/src/main.rs
@@ -1,6 +1,6 @@
 use std::io::prelude::*;
 
-fn input() -> Vec<Vec<usize>> {
+fn input() -> Vec<Vec<u8>> {
     let stdin = std::io::stdin();
     stdin
         .lock()
@@ -15,22 +15,65 @@ fn input() -> Vec<Vec<usize>> {
         .collect()
 }
 
-fn rotate(input: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+fn transpose(input: Vec<Vec<u8>>) -> Vec<Vec<u8>> {
     (0..input[0].len())
         .map(|idx| input.iter().map(|c| c[idx]).collect())
         .collect()
 }
 
-fn one(input: &Vec<Vec<usize>>) -> usize {
-    input.iter().map(|column| ());
-    
-        0
+fn one(input: &Vec<Vec<u8>>) -> usize {
+    let gamma: Vec<u8> = transpose(input.clone())
+        .iter()
+        .map(|column| {
+            column.iter().fold(
+                std::collections::HashMap::new(),
+                |mut map: std::collections::HashMap<u8, i32>, val| {
+                    *map.entry(*val).or_default() += 1;
+                    map
+                },
+            )
+        })
+        .filter_map(|frequencies| {
+            Some(
+                frequencies
+                    .into_iter()
+                    .max_by_key(|(_, val)| val.clone())?
+                    .0,
+            )
+        })
+        .collect();
+        
+    let epsilon: Vec<u8> = gamma
+        .iter()
+        .map(|bit| match bit {
+            0 => 1,
+            1 => 0,
+            _ => panic!("Not bit"),
+        })
+        .collect();
+
+    let gamma = usize::from_str_radix(
+        &gamma
+            .iter()
+            .map(|item| item.to_string())
+            .collect::<String>(),
+        2,
+    )
+    .expect("Should always be valid bit string");
+
+    let epsilon = usize::from_str_radix(
+        &epsilon
+            .iter()
+            .map(|item| item.to_string())
+            .collect::<String>(),
+        2,
+    )
+    .expect("Should always be valid bit string");
+
+    gamma * epsilon
 }
 
 fn main() {
     let input = input();
-    let rotated = rotate(input.clone());
-    println!("Input {:#?}", input);
-    println!("Rotated: {:#?}", rotated);
     println!("One {}", one(&input));
 }
